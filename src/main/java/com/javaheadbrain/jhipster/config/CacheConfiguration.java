fc647e6faa4882eb8d2ea4f158f496732134b1c5
Viktor Gamov <gamussa@gmail.com>
2016-11-15 21:00:37 -0500

    Externalized Hazelcast configuration

--- src/main/java/com/javaheadbrain/jhipster/config/CacheConfiguration.java
+++ src/main/java/com/javaheadbrain/jhipster/config/CacheConfiguration.java
@@ -1,12 +1,8 @@
 package com.javaheadbrain.jhipster.config;
 
-import com.hazelcast.config.Config;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.Hazelcast;
-import com.hazelcast.instance.HazelcastInstanceFactory;
-import com.hazelcast.config.MapConfig;
-import com.hazelcast.config.EvictionPolicy;
-import com.hazelcast.config.MaxSizeConfig;
+import com.javaheadbrain.jhipster.config.hazelcast.HazelcastConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
@@ -24,14 +20,17 @@ import javax.inject.Inject;
 @SuppressWarnings("unused")
 @Configuration
 @EnableCaching
+@AutoConfigureAfter(value = {HazelcastConfiguration.class, MetricsConfiguration.class})
 @AutoConfigureBefore(value = {DatabaseConfiguration.class, WebConfigurer.class})
-@AutoConfigureAfter(value = { MetricsConfiguration.class })
 public class CacheConfiguration {
 
     private final Logger log = LoggerFactory.getLogger(CacheConfiguration.class);
 
     @Inject
     private Environment env;
+
+    @Inject
+    private HazelcastInstance hazelcastInstance;
 
     private CacheManager cacheManager;
 
@@ -46,72 +45,6 @@ public class CacheConfiguration {
         log.debug("Starting HazelcastCacheManager");
         cacheManager = new com.hazelcast.spring.cache.HazelcastCacheManager(hazelcastInstance);
         return cacheManager;
-    }
-
-    @Bean
-    public HazelcastInstance hazelcastInstance(JHipsterProperties jHipsterProperties) {
-        log.debug("Configuring Hazelcast");
-        Config config = new Config();
-        config.setInstanceName("hzjHipster");
-        config.getNetworkConfig().setPort(5701);
-        config.getNetworkConfig().setPortAutoIncrement(true);
-
-        // In development, remove multicast auto-configuration
-        if (env.acceptsProfiles(Constants.SPRING_PROFILE_DEVELOPMENT)) {
-            System.setProperty("hazelcast.local.localAddress", "127.0.0.1");
-
-            config.getNetworkConfig().getJoin().getAwsConfig().setEnabled(false);
-            config.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(false);
-            config.getNetworkConfig().getJoin().getTcpIpConfig().setEnabled(false);
-        }
-        config.getMapConfigs().put("default", initializeDefaultMapConfig());
-        config.getMapConfigs().put("com.javaheadbrain.jhipster.domain.*", initializeDomainMapConfig(jHipsterProperties));
-        config.getMapConfigs().put("clustered-http-sessions", initializeClusteredSession(jHipsterProperties));
-
-        return Hazelcast.newHazelcastInstance(config);
-    }
-
-    private MapConfig initializeDefaultMapConfig() {
-        MapConfig mapConfig = new MapConfig();
-
-        /*
-            Number of backups. If 1 is set as the backup-count for example,
-            then all entries of the map will be copied to another JVM for
-            fail-safety. Valid numbers are 0 (no backup), 1, 2, 3.
-         */
-        mapConfig.setBackupCount(0);
-
-        /*
-            Valid values are:
-            NONE (no eviction),
-            LRU (Least Recently Used),
-            LFU (Least Frequently Used).
-            NONE is the default.
-         */
-        mapConfig.setEvictionPolicy(EvictionPolicy.LRU);
-
-        /*
-            Maximum size of the map. When max size is reached,
-            map is evicted based on the policy defined.
-            Any integer between 0 and Integer.MAX_VALUE. 0 means
-            Integer.MAX_VALUE. Default is 0.
-         */
-        mapConfig.setMaxSizeConfig(new MaxSizeConfig(0, MaxSizeConfig.MaxSizePolicy.USED_HEAP_SIZE));
-
-        return mapConfig;
-    }
-
-    private MapConfig initializeDomainMapConfig(JHipsterProperties jHipsterProperties) {
-        MapConfig mapConfig = new MapConfig();
-        mapConfig.setTimeToLiveSeconds(jHipsterProperties.getCache().getHazelcast().getTimeToLiveSeconds());
-        return mapConfig;
-    }
-
-    private MapConfig initializeClusteredSession(JHipsterProperties jHipsterProperties) {
-        MapConfig mapConfig = new MapConfig();
-        mapConfig.setBackupCount(jHipsterProperties.getCache().getHazelcast().getBackupCount());
-        mapConfig.setTimeToLiveSeconds(jHipsterProperties.getCache().getHazelcast().getTimeToLiveSeconds());
-        return mapConfig;
     }
 
     /**
